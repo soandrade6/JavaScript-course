9b956ebf2705fc3cc9efeee194df36c8da91157e
soandrade6 <sofia.andradep06@gmail.com>
2024-03-31 23:04:58 -0500

    App de listado de personas

---
+++ app-listado-personas/js/app.js
@@ -0,0 +1,27 @@
+const personas = [];
+
+function mostrarPersonas(){
+    console.log("Mostrar personas")
+    let texto = '';
+    for(let persona of personas){
+        console.log(persona);
+        texto += `<li>${persona.nombre} ${persona.apellido}</li>`;
+    }
+
+    document.getElementById("personas").innerHTML = texto;
+}
+
+function agregarPersona(){
+    const forma = document.forms["forma"];
+    const nombre = forma["nombre"];
+    const apellido = forma["apellido"];
+    if(nombre.value != '' && apellido.value != ''){
+        const persona = new Persona(nombre.value, apellido.value);
+        console.log(persona);
+        personas.push(persona)
+        mostrarPersonas();
+    }else{
+        console.log("No hay información que agregar")
+    }
+    
+}
